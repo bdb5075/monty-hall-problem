e046ead7e891635d1aaa96cdb572ab9d8eba95ba
Brittany <bdb5075@gmail.com>
2020-07-16 14:26:25 -0400

    added play  button and background image

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var doors = document.querySelectorAll('.door');
 var directions = document.getElementById("directions");
 var button = document.getElementsByClassName('selection');
+var body = document.querySelector("body");
+var playButton = document.getElementById('play-button');
 
 let doorPicked = false;
 let zonks = [];
@@ -10,6 +12,7 @@ let zonkOne;
 let zonkTwo;
 let openDoor;
 let remainingDoors = [];
+
 
 //assign the losing doors to an array
 function assignDoors() {
@@ -37,7 +40,7 @@ function assignDoors() {
 }
 
 //determine which door to open
-function openADoor() {
+setTimeout (function openADoor() {
 	//if the winning door is picked, open the zonk door that was randomly chosen as zonkOne
 	if (winner === pickedDoor) {
 		zonkOne.classList.add("goat");
@@ -58,7 +61,7 @@ function openADoor() {
 		button[i].style.visibility = "visible";
 		button[i].addEventListener("click", decisionTime);
 	}
-}
+}, 3000);
 
 
 //player picks yes or no button to switch doors
@@ -88,15 +91,19 @@ function decisionTime() {
 	//is the picked door the same as the winning door? 
 	setTimeout(function determineWinner(){
 	if(pickedDoor === winner) {
-		alert("yayyyy winner!");
+		directions.textContent = "You won!!";
+		body.style.background = "url('img/confetti.jpg') center center / cover";
 	} else {
-		alert("nooo!");
+		directions.textContent = "Sorry. Play again?"
 	}
 	winner.classList.add("winner");
 	for(var i=0; i<doors.length; i++){
 		if(doors[i] != winner) {
 		doors[i].classList.add("goat");
 	}}
+	for (var i=0; i<button.length; i++) {
+		button[i].style.visibility = "hidden";
+	}
 }, 1500);
 }
 
@@ -104,7 +111,13 @@ function decisionTime() {
 let randomNumber = Math.floor(Math.random()*3);
 let winner = doors[randomNumber];	
 
-//add event listener to doors
-for (var i=0; i<doors.length; i++) {
+//press button to play game
+playButton.addEventListener("click", function() {
+	//add event listener to doors
+	for (var i=0; i<doors.length; i++) {
 	doors[i].addEventListener("click", assignDoors);
-}
+	directions.style.visibility = "visible";
+	playButton.style.visibility = "hidden";
+	}
+})
+
